f553f3dc61238fbeb79ffde263b9ba4b5402eedf
BryanField15 <4bryanfield@gmail.com>
2023-10-03 09:35:34 +1100

    Node farm completed

---
+++ 1-node-farm/index.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
+//////////////////////////////////
+// FILES
+
+//Blocking synchronos way
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const today = new Date();
+// const formattedDate = today.toLocaleDateString('en-AU');
+// const formattedTime = today.toLocaleTimeString('en-AU', { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' });
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${formattedDate} at ${formattedTime}`;
+// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// console.log('File written')
+
+//Non-blocking asynchronos way
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error');
+//   console.log(data1);
+
+//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file');
+
+/////////////////////////////////
+// SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+console.log(slugify("Fresh Avocado", { lower: true }));
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+
+    //Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    //API page
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    //Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
